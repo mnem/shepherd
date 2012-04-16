58a07fdf54566f6187bd8fdede3c6e8fc934dfe9
Simone <simone@vizio360.co.uk>
2012-04-16 19:37:34 +0100

    WIP

--- prototypes/flocking/coffee/components/sheep.coffee
+++ prototypes/flocking/coffee/components/sheep.coffee
@@ -16,7 +16,7 @@ Crafty.c "sheep",
             #this.rotation += this.rspeed
 
 
-            minDistance = 50
+            minDistance = 70
             element =
                 x: this.x - minDistance
                 y: this.y - minDistance
@@ -51,48 +51,41 @@ Crafty.c "sheep",
                 mm = sheepX[i]
                 if(mm[0] == this[0])
                     continue
+                if Math.abs(this.direction.angleBetween(mm.direction)) > 1.57
+                    continue
+               
 
-                dx = this.x - mm.x
-                dy = this.y - mm.y
                 mmVector = new Crafty.math.Vector2D(mm.x, mm.y)
                 distance = thisVector.distance(mmVector)
                 avgDirection.add(mm.direction)
-                if(distance < 50)
-                    #console.log("too close")
+                #avgDirection.add(mmVector)
+                if(distance < 40)
                     vx = this.x - mm.x
                     vy = this.y - mm.y
                     #normalising the vector
                     vx = vx / distance
                     vy = vy / distance
-                    xds += vx
-                    yds += vy
-                    tooClose = true
-                    this.movementLengthFrames = 0
-
-                else if (distance > 500)
-                    vx = mm.x - this.x
-                    vy = mm.y - this.y
-                    #normalising the vector
-                    vx = vx / distance
-                    vy = vy / distance
-                    xds += vx
-                    yds += vy
-                    tooClose = true
-                    this.movementLengthFrames = 0
+                    mmVector.x = vx
+                    mmVector.y = vy
+                    avgDirection.add(mmVector)
 
 
 
             avgDirection.normalize()
             this.direction.x = avgDirection.x
             this.direction.y = avgDirection.y
-            avgDirection.x += xds
-            avgDirection.y += yds
-            avgDirection.normalize()
+            this.xspeed = this.direction.x
+            this.yspeed = this.direction.y
+            zeroV = new Crafty.math.Vector2D(1,0)
+            this.rotation = this.direction.angleBetween(zeroV) * 180 / Math.PI
 
+            ###
             if(tooClose)
                 this.xspeed = xds
                 this.yspeed = yds
 
+            ###
+            ###
             if (!tooClose && this.movementLengthFrames == 0)
                 this.xspeed = 0
                 this.yspeed = 0
@@ -103,21 +96,19 @@ Crafty.c "sheep",
                     this.yspeed = Crafty.math.randomInt(-1, 1)
                     this.direction.x = this.xspeed
                     this.direction.y = this.yspeed
+            ###
 
             this.x += this.xspeed
             this.y += this.yspeed
-            if(this.movementLengthFrames > 0)
-                this.movementLengthFrames = this.movementLengthFrames - 1
 
-            if(this._x > Crafty.viewport.width)
-                this.direction.x = this.xspeed = -1
-					
-            if(this._x < -64)
-                this.direction.x = this.xspeed = 1
-            if(this._y > Crafty.viewport.height)
-                this.direction.y = this.yspeed = -1
-            if(this._y < 0)
-                this.direction.y = this.yspeed = 1
+            if(this.x > Crafty.viewport.width-50)
+                this.x = 0
+            if(this.x < 0)
+                this.x = Crafty.viewport.width-50 
+            if(this.y > Crafty.viewport.height-50)
+                this.y = 0
+            if(this.y < 0)
+                this.y = Crafty.viewport.height-50
 
             # remove sheep from ordered arrays
             # need to insert it again
--- prototypes/flocking/public/js/game.js
+++ prototypes/flocking/public/js/game.js
@@ -13,8 +13,8 @@
         movementLengthFrames: 0,
         direction: new Crafty.math.Vector2D(Crafty.math.randomInt(-1, 1), Crafty.math.randomInt(-1, 1))
       }).bind("EnterFrame", function() {
-        var avgDirection, currentIndex, distance, distantSheep, dx, dy, element, highIndex, i, lowIndex, minDistance, mm, mmVector, sheepX, sheepY, thisVector, tooClose, vx, vy, xds, yds, _ref, _ref2, _ref3;
-        minDistance = 50;
+        var avgDirection, currentIndex, distance, distantSheep, element, highIndex, i, lowIndex, minDistance, mm, mmVector, sheepX, sheepY, thisVector, tooClose, vx, vy, xds, yds, zeroV, _ref, _ref2, _ref3;
+        minDistance = 70;
         element = {
           x: this.x - minDistance,
           y: this.y - minDistance
@@ -37,61 +37,50 @@
           if (sheepY.indexOf(sheepX[i]) === -1) continue;
           mm = sheepX[i];
           if (mm[0] === this[0]) continue;
-          dx = this.x - mm.x;
-          dy = this.y - mm.y;
+          if (Math.abs(this.direction.angleBetween(mm.direction)) > 1.57) continue;
           mmVector = new Crafty.math.Vector2D(mm.x, mm.y);
           distance = thisVector.distance(mmVector);
           avgDirection.add(mm.direction);
-          if (distance < 50) {
+          if (distance < 40) {
             vx = this.x - mm.x;
             vy = this.y - mm.y;
             vx = vx / distance;
             vy = vy / distance;
-            xds += vx;
-            yds += vy;
-            tooClose = true;
-            this.movementLengthFrames = 0;
-          } else if (distance > 500) {
-            vx = mm.x - this.x;
-            vy = mm.y - this.y;
-            vx = vx / distance;
-            vy = vy / distance;
-            xds += vx;
-            yds += vy;
-            tooClose = true;
-            this.movementLengthFrames = 0;
+            mmVector.x = vx;
+            mmVector.y = vy;
+            avgDirection.add(mmVector);
           }
         }
         avgDirection.normalize();
         this.direction.x = avgDirection.x;
         this.direction.y = avgDirection.y;
-        avgDirection.x += xds;
-        avgDirection.y += yds;
-        avgDirection.normalize();
-        if (tooClose) {
-          this.xspeed = xds;
-          this.yspeed = yds;
-        }
-        if (!tooClose && this.movementLengthFrames === 0) {
-          this.xspeed = 0;
-          this.yspeed = 0;
-          if (Crafty.math.randomInt(1, 1000) > 999) {
-            this.movementLengthFrames = Crafty.math.randomInt(50, 300);
-            this.xspeed = Crafty.math.randomInt(-1, 1);
-            this.yspeed = Crafty.math.randomInt(-1, 1);
-            this.direction.x = this.xspeed;
-            this.direction.y = this.yspeed;
-          }
-        }
+        this.xspeed = this.direction.x;
+        this.yspeed = this.direction.y;
+        zeroV = new Crafty.math.Vector2D(1, 0);
+        this.rotation = this.direction.angleBetween(zeroV) * 180 / Math.PI;
+        /*
+                    if(tooClose)
+                        this.xspeed = xds
+                        this.yspeed = yds
+        */
+        /*
+                    if (!tooClose && this.movementLengthFrames == 0)
+                        this.xspeed = 0
+                        this.yspeed = 0
+        
+                        if (Crafty.math.randomInt(1, 1000) > 999)
+                            this.movementLengthFrames = Crafty.math.randomInt(50, 300)
+                            this.xspeed = Crafty.math.randomInt(-1, 1)
+                            this.yspeed = Crafty.math.randomInt(-1, 1)
+                            this.direction.x = this.xspeed
+                            this.direction.y = this.yspeed
+        */
         this.x += this.xspeed;
         this.y += this.yspeed;
-        if (this.movementLengthFrames > 0) {
-          this.movementLengthFrames = this.movementLengthFrames - 1;
-        }
-        if (this._x > Crafty.viewport.width) this.direction.x = this.xspeed = -1;
-        if (this._x < -64) this.direction.x = this.xspeed = 1;
-        if (this._y > Crafty.viewport.height) this.direction.y = this.yspeed = -1;
-        if (this._y < 0) this.direction.y = this.yspeed = 1;
+        if (this.x > Crafty.viewport.width - 50) this.x = 0;
+        if (this.x < 0) this.x = Crafty.viewport.width - 50;
+        if (this.y > Crafty.viewport.height - 50) this.y = 0;
+        if (this.y < 0) this.y = Crafty.viewport.height - 50;
         currentIndex = Crafty.orderedSheepX.indexOf(this);
         [].splice.apply(Crafty.orderedSheepX, [currentIndex, currentIndex - currentIndex + 1].concat(_ref2 = [])), _ref2;
         currentIndex = Crafty.orderedSheepY.indexOf(this);
